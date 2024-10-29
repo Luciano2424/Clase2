a1b1568ab41499066f7edf8d52dd88c318667fbc
Luciano2424 <emprendedores.cl24@gmail.com>
2024-10-29 15:22:20 -0300

    Add files via upload

---
+++ Clase 2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Mi primera aplicación de Streamlit")
+
+st.sidebar.title("Barra lateral")
+st.sidebar.header("Hola")
+st.sidebar.write("Esto es mi barra lateral")
+
+st.sidebar.image("uss.png")
+
+if st.sidebar.button("Haz echo cick pero en la barra lateral"):
+    st.sidebar.write("Haz echo click en el botón de la barra lateral")
+
+user_input = st.sidebar.text_input("Escribe algo en la barra: ")
+st.sidebar.write(f"Has escrito: {user_input}")
